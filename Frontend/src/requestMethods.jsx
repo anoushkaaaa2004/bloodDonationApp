642d0f8d6115fbff3aa9e5ad4688be101770fe43
Anoushka Deb <147077098+anoushkaaaa2004@users.noreply.github.com>
2024-10-15 21:23:15 +0530

    Delete Frontend directory

--- Frontend/src/requestMethods.jsx
+++
@@ -1,8 +0,0 @@
-// src/requestMethods.js
-import axios from 'axios';
-
-const BASE_URL = "http://localhost:5000/api"; // Adjust this to your backend URL
-
-export const publicRequest = axios.create({
-  baseURL: BASE_URL,
-});
